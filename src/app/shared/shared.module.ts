c4db94c288c191b22abbedae9a8994e482750419
Lucas Teixeira <lucasteixeira.dev@gmail.com>
2020-11-12 19:33:34 -0300

    Component padrão para card

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material-module/material-module.module';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CardComponent],
   imports: [
     CommonModule,
     MaterialModule
   ],
   exports: [
-    MaterialModule
+    MaterialModule,
+    CardComponent
   ]
 })
 export class SharedModule { }
