42af8bebf0df1f2dfeedd89c4de6201518a13c70
Lucas Coutinho <lucascgs1@hotmail.com>
2020-11-13 20:50:33 -0300

    ordenacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './component/header/header.component';
 
+//module
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 //package
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
-import { AppRoutingModule } from './app-routing.module';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
@@ -17,7 +18,6 @@ const appRoutes: Routes = [
     path: 'home',
     component: HomeComponent
   },
-
   {
     path: '',
     redirectTo: '/home',
